43311efe62ea04548f1611c3abf8b761afa2ce68
Eric Cosulea <ericcosulea@Erics-MacBook-Air.local>
2026-04-21 01:25:37 +0300

    Allow new typography and text fields in update-appearance API

--- api/update-appearance.php
+++ api/update-appearance.php
@@ -14,13 +14,15 @@ if (!is_authenticated()) {
 $input = json_decode(file_get_contents('php://input'), true);
 $settings = load_settings();
 
-$allowed = ['site_title', 'site_subtitle', 'site_footer', 'color_bg', 'color_surface', 'color_accent', 'color_text', 'color_text_muted', 'heading_size'];
+$allowed = ['site_title', 'site_subtitle', 'site_footer', 'color_bg', 'color_surface', 'color_accent', 'color_text', 'color_text_muted', 'heading_size', 'site_desc', 'nl_title', 'nl_desc', 'desc_size_desktop', 'desc_size_mobile', 'nl_title_size_desktop', 'nl_title_size_mobile', 'nl_desc_size_desktop', 'nl_desc_size_mobile'];
 
 foreach ($allowed as $key) {
     if (isset($input[$key])) {
         $val = trim($input[$key]);
         if (str_starts_with($key, 'color_') && !preg_match('/^#[0-9a-fA-F]{6}$/', $val)) continue;
-        if ($key === 'heading_size' && (!is_numeric($val) || $val < 20 || $val > 60)) continue;
+        if (str_ends_with($key, '_size') || str_ends_with($key, '_size_desktop') || str_ends_with($key, '_size_mobile')) {
+            if (!is_numeric($val) || $val < 8 || $val > 100) continue;
+        }
         $settings[$key] = $val;
     }
 }
